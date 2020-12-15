5976e916c42e7f2a58f435cf7c6dc6467b3ea24a
Xutrurn <kanjoskillz@gmail.com>
2020-12-15 18:05:12 +0100

    added ticket viz and hidden on click

--- js/script.js
+++ js/script.js
@@ -1,40 +1,58 @@
 // alert("Hello world");
 
 var genera = document.getElementById("genera");
-genera.addEventListener("click", function()
-{
+genera.addEventListener("click", function() {
 
-var userInfo = document.getElementById("userInfo").value;
-var km = document.getElementById("km").value;
-var age = document.getElementById("age").value;
-console.log(age);
+  var userInfo = document.getElementById("userInfo").value;
+  var km = document.getElementById("km").value;
+  var age = document.getElementById("age").value;
+  console.log(age);
 
-var fullPrice = km * 0.21;
-var offer = "Maggiorenne"
-
-
-if (age == "juniorPrice") {
-  fullPrice -= fullPrice * .2;
-  offer = "Minorenne"
-} else if (age == "seniorPrice") {
-  fullPrice -= fullPrice * .4;
-  offer = "Over"
-}
-
-// random
-document.getElementById("carriage").innerHTML = Math.floor(Math.random() * 9) + 1;
-document.getElementById("raceCode").innerHTML = Math.floor(Math.random() * 10000) + 90001;
+  var fullPrice = km * 0.21;
+  var offer = "Maggiorenne"
 
 
-console.log(fullPrice.toFixed(2));
-console.log(userInfo, km, age);
+  if (age == "juniorPrice") {
+    fullPrice -= fullPrice * .2;
+    offer = "Minorenne"
+  } else if (age == "seniorPrice") {
+    fullPrice -= fullPrice * .4;
+    offer = "Over"
+  }
+
+  // random
+  document.getElementById("carriage").innerHTML = Math.floor(Math.random() * 9) + 1;
+  document.getElementById("raceCode").innerHTML = Math.floor(Math.random() * 10000) + 90001;
 
 
-// write values
-document.getElementById("show-name").innerHTML = userInfo;
-document.getElementById("offer").innerHTML = offer;
-document.getElementById("fullPrice").innerHTML = fullPrice.toFixed(2) + "€";
+  console.log(fullPrice.toFixed(2));
+  console.log(userInfo, km, age);
 
+
+  // write values
+  document.getElementById("show-name").innerHTML = userInfo;
+  document.getElementById("offer").innerHTML = offer;
+  document.getElementById("fullPrice").innerHTML = fullPrice.toFixed(2) + "€";
+
+  var biglietto = document.getElementById("biglietto");
+  biglietto.style.visibility = "visible";
+
+});
+
+var annulla = document.getElementById("annulla");
+annulla.addEventListener("click", function() {
+
+  document.getElementById("userInfo").innerHTML = "";
+  document.getElementById("km").innerHTML = "";
+  document.getElementById("carriage").innerHTML = "";
+  document.getElementById("raceCode").innerHTML = "";
+  document.getElementById("show-name").innerHTML = "";
+  document.getElementById("offer").innerHTML = "";
+  document.getElementById("fullPrice").innerHTML = "";
+
+
+  var biglietto = document.getElementById("biglietto");
+  biglietto.style.visibility = "hidden";
 
 
 
