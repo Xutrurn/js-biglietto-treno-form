65def490ce3a8356c07476384e45809c99518fe4
Xutrurn <kanjoskillz@gmail.com>
2020-12-15 16:22:18 +0100

    js testing

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 // alert("Hello world");
 
+var userInfo = document.getElementById("userInfo").value;
+var km = document.getElementById("km").value;
+var age = document.getElementById("age").value;
 
 
-
-
-
-// console.log();
+// write values
+document.getElementById("show-name").innerHTML = userInfo;
+console.log(userInfo, km, age);
